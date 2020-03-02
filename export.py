2034a9ed24074b733512aac6c2f3a5d8f6a6756a
eddiez9 <eddie@eddiez.me>
2020-03-02 20:53:30 +1100

    data grabbed for scan runs

--- export.py
+++ export.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 import configparser
 import requests
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
 import pymysql.cursors
+
+# Disable SSL warnings
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 # Read configuration
 config = configparser.ConfigParser()
@@ -23,10 +27,14 @@ database = config.get('mysql','database')
 # Nessus endpoints
 FOLDERS = '/folders'
 SCANS = '/scans'
+
 SCAN_ID = SCANS + '/{scan_id}'
-SCAN_RUN_ID = SCAN_ID + '?history_id={history_id}'
-HOST_VULN = SCAN_ID + '/hosts/{host_id}'
-PLUGINS = HOST_VULN + '/plugins/{plugin_id}'
+HOST_ID = SCAN_ID + '/hosts/{host_id}'
+PLUGIN_ID = HOST_ID + '/plugins/{plugin_id}'
+
+SCAN_RUN = SCAN_ID + '?history_id={history_id}'
+HOST_VULN = HOST_ID + '?history_id={history_id}'
+PLUGIN_OUTPUT = PLUGIN_ID + '?history_id={history_id}'
 
 # Database connection
 connection = pymysql.connect(host=db_hostname,
@@ -50,16 +58,23 @@ def get_folders():
 def get_scans():
     return request(SCANS)
 
-def get_scan_id(id):
-    return request(SCAN_ID.format(scan_id=id))
+def get_scan(scan_id):
+    return request(SCAN_ID.format(scan_id=scan_id))
 
-def get_scan_run_id(scan_id, history_id):
-    return request(SCAN_RUN_ID.format(scan_id=scan_id, history_id=history_id))
+def get_scan_run(scan_id, history_id):
+    return request(SCAN_RUN.format(scan_id=scan_id, history_id=history_id))
+
+def get_host_vuln(scan_id, host_id, history_id):
+    return request(HOST_VULN.format(scan_id=scan_id, host_id=host_id, history_id=history_id))
+
+def get_plugin_output(scan_id, host_id, plugin_id, history_id):
+    return request(PLUGIN_OUTPUT.format(scan_id=scan_id, host_id=host_id, plugin_id=plugin_id, history_id=history_id))
 
 # Nessus export functions
 def update_folders():
     folders = get_folders()
     with connection.cursor() as cursor:
+        # Upsert folders
         for folder in folders['folders']:
             sql = "INSERT INTO `folder` (`folder_id`, `type`, `name`)\
                     VALUES (%s, %s, %s)\
@@ -67,17 +82,48 @@ def update_folders():
             cursor.execute(sql, (folder['id'], folder['type'], folder['name'], folder['type'], folder['name']))
     connection.commit()
 
+
+
+# def insert_host_vuln(host_id, scan_run_id, plugin_id, cursor):
+
+def insert_host(scan_id, host_id, history_id, cursor):
+    host = get_host_vuln(scan_id, host_id, history_id)
+
+    # Count number of vulns of each severity for this host in this scan run
+    # 0 is informational, 4 is critical
+    sev_count = [0] * 5
+    for vuln in host['vulnerabilities']:
+        sev_count[vuln['severity']] += vuln['count']
+        
+    if (host_id == 5):
+        print(sev_count)
+
+
 def insert_scan_run(scan_id, history_id):
-    scan_run = get_scan_run_id(scan_id, history_id)
+    scan_run = get_scan_run(scan_id, history_id)
+
+    # Count number of vulns of each severity for this scan run
+    # 0 is informational, 4 is critical
+    sev_count = [0] * 5
+    for vuln in scan_run['vulnerabilities']:
+        sev_count[vuln['severity']] += vuln['count']
 
     with connection.cursor() as cursor:
+        # Insert scan run details
+        sql = "INSERT INTO `scan_run` (`scan_run_id`, `scan_id`, `scan_start`, `scan_end`, `targets`, `host_count`, `critical_count`, `high_count`, `medium_count`, `low_count`, `info_count`)\
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        # cursor.execute(sql, (history_id, scan_id, scan_run['info']['scanner_start'], scan_run['info']['scanner_end'], scan_run['info']['targets'], scan_run['info']['hostcount'], sev_count[4], sev_count[3], sev_count[2], sev_count[1], sev_count[0]))
+        
+        # Insert hosts in scan run
+        for host in scan_run['hosts']:
+            insert_host(scan_id, host['host_id'], history_id, cursor)
 
-    
     connection.commit()
 
 def update_scans():
     scans = get_scans()
     with connection.cursor() as cursor:
+        # Upsert scans
         for scan in scans['scans']:
             sql = "INSERT INTO `scan` (`scan_id`, `folder_id`, `type`, `name`)\
                     VALUES (%s, %s, %s, %s)\
@@ -86,13 +132,15 @@ def update_scans():
     connection.commit()
 
     for scan in scans['scans']:
+        print ('Processing:' + scan['name'])
+        
         # Retreive details about the current scan
-        scan_details = get_scan_id(scan['id'])
+        scan_details = get_scan(scan['id'])
 
         # Check each run of each scan
         for scan_run in scan_details['history']:
             # If the scan has finished
-            if (scan_run['status'] != 'running') OR (scan_run['status'] != 'paused'):
+            if ((scan_run['status'] != 'running') or (scan_run['status'] != 'paused')):
                 # TODO If we haven't already saved this scan run
                 insert_scan_run(scan['id'], scan_run['history_id'])
 
